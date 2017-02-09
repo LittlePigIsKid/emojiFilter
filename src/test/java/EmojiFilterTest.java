e7e5177898f6c7e825968733855792a909859505
stove.shi <stove.shi@yeahmobi.com>
2017-02-09 15:46:49 +0800

    test something

---
+++ src/test/java/EmojiFilterTest.java
@@ -0,0 +1,13 @@
+import com.http.GetEmojiUTF8;
+import org.junit.Test;
+
+/**
+ * Created by 1111 on 2017/2/9.
+ */
+public class EmojiFilterTest {
+    @Test
+    public void testEmojiFiter(){
+        //String
+        GetEmojiUTF8 getEmojiUTF8 = new GetEmojiUTF8();
+    }
+}
